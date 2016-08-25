24c920cc13002ff9c04edae382892489678d8603
pehas <pehas.90@gmail.com>
2016-08-25 21:01:40 +0600

    Add admin

--- first_app/article/admin.py
+++ first_app/article/admin.py
@@ -1,6 +1,18 @@
 from article.models import Comments, Article
 from django.contrib import admin
 
+def make_published(modeladmin, request, queryset):
+    for q in queryset:
+        q.title = q.title + q.title
+        q.save()
+make_published.short_description = "Change title"
+
 # Register your models here.
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'body')
+    fields = ('body',)
+    list_filter = ('title',)
+    search_fields = ['title', ]
+    actions = [make_published]
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comments)
--- first_app/article/views.py
+++ first_app/article/views.py
@@ -1,13 +1,28 @@
 from article.forms import CommentForm
 from article.models import Article, Comments
 from django.shortcuts import render, get_object_or_404
-
+from django.views.generic import TemplateView
 
 # Create your views here.
 def index(request):
     articles = Article.objects.all()
+    if 'q' in request.GET:
+        articles=articles.filter(title=request.GET['q'])
     return render(request, 'article/index.html', {'articles': articles})
 
+class ArticleView(TemplateView):
+    template_name = 'article/inner_page.html'
+    def get_context_data(self, id, **kwargs):
+        kwargs['form']= CommentForm()
+        kwargs['article']= get_object_or_404(Article, id=id)
+
+    def post(self, request, id):
+        article = get_object_or_404(Article, id=id)
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.article = article
+            form.save()
 
 def get_article(request, id):
     article = get_object_or_404(Article, id=id)
